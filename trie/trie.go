05a893cf3a6e09329647a64678e7b1e078108c46
Viet Pham <vietpham102301@gmail.com>
2024-01-31 13:30:39 +0700

    added trie

---
+++ trie/trie.go
@@ -0,0 +1,60 @@
+package main
+
+import "fmt"
+
+const AlphabetSize = 26
+
+type Node struct {
+	children [AlphabetSize]*Node
+	isEnd    bool
+}
+
+type Trie struct {
+	root *Node
+}
+
+func InitializeTrie() *Trie {
+	return &Trie{root: &Node{}}
+}
+
+func (t *Trie) Insert(w string) {
+	wordLength := len(w)
+	currentNode := t.root
+	for i := 0; i < wordLength; i++ {
+		charIndex := w[i] - 'a'
+		if currentNode.children[charIndex] == nil {
+			currentNode.children[charIndex] = &Node{}
+		}
+		currentNode = currentNode.children[charIndex]
+	}
+	currentNode.isEnd = true
+}
+
+func (t *Trie) Search(w string) bool {
+	wordLength := len(w)
+	currentNode := t.root
+	for i := 0; i < wordLength; i++ {
+		charIndex := w[i] - 'a'
+		if currentNode.children[charIndex] == nil {
+			return false
+		}
+		currentNode = currentNode.children[charIndex]
+	}
+	if currentNode.isEnd == true {
+		return true
+	}
+
+	return false
+}
+
+func main() {
+	trie := InitializeTrie()
+
+	trie.Insert("viet")
+	trie.Insert("vietnam")
+
+	fmt.Println(trie)
+
+	fmt.Println(trie.Search("smt"))
+
+}
