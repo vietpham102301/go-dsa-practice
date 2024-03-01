84e4f357763cbd9c1287750ca20caba0fce15a3b
Viet Pham <vietpham102301@gmail.com>
2024-03-01 16:12:21 +0700

    added jump_game_ii more optimal solution

--- algorithms/jump_game_ii/jump_game_ii.go
+++ algorithms/jump_game_ii/jump_game_ii.go
@@ -2,28 +2,50 @@ package main
 
 import "fmt"
 
+//func jump(nums []int) int {
+//	goal := len(nums) - 1
+//	count := 0
+//	for goal != 0 {
+//		goal = findMaxStepIndex(nums, goal)
+//		count++
+//	}
+//
+//	return count
+//}
+//
+//func findMaxStepIndex(nums []int, goal int) int {
+//	maxStep := 0
+//	indexFound := -1
+//	for i := len(nums) - 2; i >= 0; i-- {
+//		if i+nums[i] >= goal && (goal-i) > maxStep {
+//			maxStep = goal - i
+//			indexFound = i
+//		}
+//	}
+//
+//	return indexFound
+//}
+
 func jump(nums []int) int {
-	goal := len(nums) - 1
+	for i := 1; i < len(nums); i++ {
+		nums[i] = max(nums[i]+i, nums[i-1])
+	}
+
 	count := 0
-	for goal != 0 {
-		goal = findMaxStepIndex(nums, goal)
+	idx := 0
+	for idx < len(nums)-1 {
 		count++
+		idx = nums[idx]
 	}
 
 	return count
 }
 
-func findMaxStepIndex(nums []int, goal int) int {
-	maxStep := 0
-	indexFound := -1
-	for i := len(nums) - 2; i >= 0; i-- {
-		if i+nums[i] >= goal && (goal-i) > maxStep {
-			maxStep = goal - i
-			indexFound = i
-		}
+func max(a int, b int) int {
+	if a > b {
+		return a
 	}
-
-	return indexFound
+	return b
 }
 
 func main() {
