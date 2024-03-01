4f7c8fefca4fc2e250b355438bc9f2f98a606251
Viet Pham <vietpham102301@gmail.com>
2024-03-01 18:07:48 +0700

    added jump_game_ii dp approach

--- algorithms/jump_game_ii/jump_game_ii.go
+++ algorithms/jump_game_ii/jump_game_ii.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 //func jump(nums []int) int {
 //	goal := len(nums) - 1
@@ -26,25 +29,53 @@ import "fmt"
 //	return indexFound
 //}
 
+//func jump(nums []int) int {
+//	for i := 1; i < len(nums); i++ {
+//		nums[i] = max(nums[i]+i, nums[i-1])
+//	}
+//
+//	count := 0
+//	idx := 0
+//	for idx < len(nums)-1 {
+//		count++
+//		idx = nums[idx]
+//	}
+//
+//	return count
+//}
+//
+//func max(a int, b int) int {
+//	if a > b {
+//		return a
+//	}
+//	return b
+//}
+
 func jump(nums []int) int {
+	if len(nums) <= 1 {
+		return 0
+	}
+
+	minimalJump := make([]int, len(nums))
+	minimalJump[0] = 0
+
 	for i := 1; i < len(nums); i++ {
-		nums[i] = max(nums[i]+i, nums[i-1])
+		minimalJump[i] = math.MaxInt
+		for j := 0; j < i; j++ {
+			if j+nums[j] >= i {
+				minimalJump[i] = min(minimalJump[i], minimalJump[j]+1)
+			}
+		}
 	}
 
-	count := 0
-	idx := 0
-	for idx < len(nums)-1 {
-		count++
-		idx = nums[idx]
-	}
-
-	return count
+	return minimalJump[len(nums)-1]
 }
 
-func max(a int, b int) int {
-	if a > b {
+func min(a int, b int) int {
+	if a < b {
 		return a
 	}
+
 	return b
 }
 
