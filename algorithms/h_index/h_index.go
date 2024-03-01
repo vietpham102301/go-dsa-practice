56f469c14eaf01ef0cdb26b195f508dd8ebc1c88
Viet Pham <vietpham102301@gmail.com>
2024-03-01 15:26:54 +0700

    added h-index more optimal solution

--- algorithms/h_index/h_index.go
+++ algorithms/h_index/h_index.go
@@ -5,26 +5,42 @@ import (
 	"sort"
 )
 
+//hIndex intuition method
+//func hIndex(citations []int) int {
+//	sort.Slice(citations, func(i, j int) bool {
+//		return citations[i] > citations[j]
+//	})
+//	h := -1
+//	for i := 1; i <= len(citations); i++ {
+//		count := 0
+//		for j := 0; j < i; j++ {
+//			if citations[j] >= i {
+//				count++
+//			}
+//		}
+//		if count > h {
+//			h = count
+//		}
+//	}
+//	return h
+//}
+
 func hIndex(citations []int) int {
-	sort.Slice(citations, func(i, j int) bool {
-		return citations[i] > citations[j]
-	})
-	h := -1
-	for i := 1; i <= len(citations); i++ {
-		count := 0
-		for j := 0; j < i; j++ {
-			if citations[j] >= i {
-				count++
-			}
+	sort.Ints(citations)
+	ans := len(citations)
+	for i := 0; i < len(citations); i++ {
+		if citations[i] >= ans {
+			break
 		}
-		if count > h {
-			h = count
-		}
+
+		ans--
 	}
-	return h
+
+	return ans
 }
 
 func main() {
 	testCase := []int{3, 0, 6, 1, 5}
 	fmt.Println(hIndex(testCase))
+
 }
