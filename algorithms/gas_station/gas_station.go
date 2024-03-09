206c35cc3806b73ab4450152f8f2e21f627abf03
Viet Pham <vietpham102301@gmail.com>
2024-03-09 15:12:16 +0700

    added more optimal solution for leetcode 134

--- algorithms/gas_station/gas_station.go
+++ algorithms/gas_station/gas_station.go
@@ -2,33 +2,56 @@ package main
 
 import "fmt"
 
+//func canCompleteCircuit(gas []int, cost []int) int {
+//	l := len(gas)
+//	for i := 0; i < l; i++ {
+//		tank := 0
+//		j := i
+//		secondTime := false
+//		itCan := true
+//		for j != i || !secondTime {
+//			tank = tank + gas[j] - cost[j]
+//			j++
+//			if j > l-1 {
+//				j = j % l
+//			}
+//			if j == i && tank >= 0 {
+//				secondTime = true
+//				continue
+//			}
+//			if tank <= 0 {
+//				itCan = false
+//				break
+//			}
+//		}
+//		if !itCan {
+//			continue
+//		}
+//		return i
+//	}
+//	return -1
+//}
+
 func canCompleteCircuit(gas []int, cost []int) int {
 	l := len(gas)
+	startingPoint := 0
+	totalFuelAndConsumsion := 0
+	totalFuelFromStartIndex := 0
+
 	for i := 0; i < l; i++ {
-		tank := 0
-		j := i
-		secondTime := false
-		itCan := true
-		for j != i || !secondTime {
-			tank = tank + gas[j] - cost[j]
-			j++
-			if j > l-1 {
-				j = j % l
-			}
-			if j == i && tank >= 0 {
-				secondTime = true
-				continue
-			}
-			if tank <= 0 {
-				itCan = false
-				break
-			}
+		totalFuelAndConsumsion += gas[i] - cost[i]
+		totalFuelFromStartIndex += gas[i] - cost[i]
+
+		if totalFuelFromStartIndex < 0 {
+			startingPoint = i + 1
+			totalFuelFromStartIndex = 0
 		}
-		if !itCan {
-			continue
-		}
-		return i
 	}
+
+	if totalFuelAndConsumsion >= 0 {
+		return startingPoint
+	}
+
 	return -1
 }
 
