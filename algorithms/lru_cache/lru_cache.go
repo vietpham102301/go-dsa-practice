c5a42c8838121d60443e93d12bcab2c286b54ebb
Viet Pham <vietpham102301@gmail.com>
2025-07-12 15:20:09 +0700

    ref: optimize lru cache

--- algorithms/lru_cache/lru_cache.go
+++ algorithms/lru_cache/lru_cache.go
@@ -4,129 +4,105 @@ import "fmt"
 
 type Node struct {
 	key, value int
-	next, prev *Node
+	prev, next *Node
 }
 
 type LRUCache struct {
-	capacity   int
-	cacheMap   map[int]*Node
-	head, tail *Node
-}
-
-func (l *LRUCache) printList() {
-	curr := l.head
-	for curr != nil {
-		fmt.Printf("key: %d, val: %d ", curr.key, curr.value)
-		curr = curr.next
-	}
-}
-
-func (l *LRUCache) moveToTail() {
-	//todo: implement this
+	capacity int
+	cache    map[int]*Node
+	head     *Node
+	tail     *Node
 }
 
 func NewLRUCache(capacity int) *LRUCache {
 	return &LRUCache{
 		capacity: capacity,
-		cacheMap: make(map[int]*Node),
+		cache:    make(map[int]*Node),
 	}
+}
+
+func (l *LRUCache) removeNode(node *Node) {
+	if node.prev != nil {
+		node.prev.next = node.next
+	} else {
+		l.head = node.next
+	}
+	if node.next != nil {
+		node.next.prev = node.prev
+	} else {
+		l.tail = node.prev
+	}
+}
+
+func (l *LRUCache) addToTail(node *Node) {
+	node.prev = l.tail
+	node.next = nil
+	if l.tail != nil {
+		l.tail.next = node
+	}
+	l.tail = node
+	if l.head == nil {
+		l.head = node
+	}
+}
+
+func (l *LRUCache) moveToTail(node *Node) {
+	if node == l.tail {
+		return
+	}
+	l.removeNode(node)
+	l.addToTail(node)
 }
 
 func (l *LRUCache) Put(key, value int) {
-	if l.cacheMap[key] == nil && l.capacity > 0 {
-		l.capacity--
-		if l.head == nil || l.tail == nil {
-			l.head = &Node{key: key, value: value, next: nil, prev: nil}
-			l.tail = l.head
-
-		} else {
-			l.tail.next = &Node{key, value, nil, l.tail}
-			l.tail = l.tail.next
-		}
-		l.cacheMap[key] = l.tail
-		return
-	} else if l.cacheMap[key] == nil && l.capacity == 0 {
-		l.head.next.prev = nil
-		l.head = l.head.next
-
-		l.tail.next = &Node{key: key, value: value, next: nil, prev: l.tail}
-		l.tail = l.tail.next
-		l.cacheMap[key] = l.tail
+	if node, exists := l.cache[key]; exists {
+		node.value = value
+		l.moveToTail(node)
 		return
 	}
 
-	refNode := l.cacheMap[key]
-	if refNode != nil {
-		refNode.value = value
-		if refNode == l.tail {
-			return
-		}
-
-		if refNode == l.head {
-			l.head = l.head.next
-			l.head.prev = nil
-		}
-
-		l.tail.next = refNode
-		refNode.prev = l.tail
-		refNode.next = nil
-		l.tail = l.tail.next
+	if len(l.cache) == l.capacity {
+		delete(l.cache, l.head.key)
+		l.removeNode(l.head)
 	}
+
+	newNode := &Node{key: key, value: value}
+	l.addToTail(newNode)
+	l.cache[key] = newNode
 }
 
 func (l *LRUCache) Get(key int) int {
-	refNode := l.cacheMap[key]
-	if refNode == nil {
-		return -1
+	if node, exists := l.cache[key]; exists {
+		l.moveToTail(node)
+		return node.value
 	}
+	return -1
+}
 
-	if refNode == l.tail {
-		return refNode.value
+func (l *LRUCache) printList() {
+	for curr := l.head; curr != nil; curr = curr.next {
+		fmt.Printf("key: %d, val: %d ", curr.key, curr.value)
 	}
-
-	if refNode == l.head {
-		l.head = l.head.next
-		l.head.prev = nil
-	}
-
-	l.tail.next = refNode
-	refNode.prev = l.tail
-	refNode.next = nil
-	l.tail = l.tail.next
-
-	return refNode.value
+	fmt.Println()
 }
 
 func main() {
 	cache := NewLRUCache(3)
 
-	// add when not reached capacity
 	cache.Put(1, 200)
 	cache.Put(2, 500)
 	cache.Put(3, 600)
 	cache.printList()
 
-	fmt.Println()
-
-	//evict when reach the capacity no duplicate key
 	cache.Put(4, 700)
 	cache.printList()
 
-	fmt.Println()
-
-	//update the recently used list when put duplicate key
 	cache.Put(2, 800)
 	cache.printList()
 
-	fmt.Println()
-	//get the key: 3
 	cache.Get(3)
 	cache.printList()
 
-	fmt.Println()
-
-	//get the key doesn't contain in the cache
 	res := cache.Get(10)
 	fmt.Println(res)
-
 }
