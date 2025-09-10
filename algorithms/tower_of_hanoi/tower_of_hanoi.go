90b0a09c370264bc0f6dfb2329fe0ea38191dc0a
Viet Pham <vietpham102301@gmail.com>
2025-09-10 09:42:01 +0700

    ref: update tower of hanoi

--- algorithms/tower_of_hanoi/tower_of_hanoi.go
+++ algorithms/tower_of_hanoi/tower_of_hanoi.go
@@ -2,21 +2,25 @@ package main
 
 import "fmt"
 
-func towerOfHanoi(stepCount *int, n, source, target, auxiliary int) {
-	*stepCount++
-	if n == 1 {
-		fmt.Printf("Move disk %d from %d to %d \n", n, source, target)
+func towerOfHanoi(count *int, diskNum int, source, target, sub int) {
+	*count++
+	if diskNum == 1 {
+		printMove(diskNum, source, target)
 		return
 	}
 
-	towerOfHanoi(stepCount, n-1, source, auxiliary, target)
-	fmt.Printf("Move disk %d from %d to %d \n", n, source, target)
-	towerOfHanoi(stepCount, n-1, auxiliary, target, source)
+	towerOfHanoi(count, diskNum-1, source, sub, target)
+	printMove(diskNum, source, target)
+
+	towerOfHanoi(count, diskNum-1, sub, target, source)
+}
+
+func printMove(disk, from, to int) {
+	fmt.Printf("move disk %d from %d to %d\n", disk, from, to)
 }
 
 func main() {
-	stepCount := new(int)
-	*stepCount = 0
-	towerOfHanoi(stepCount, 5, 1, 3, 2)
-	fmt.Printf("steps count: %d\n", *stepCount)
+	count := 0
+	towerOfHanoi(&count, 3, 1, 3, 2)
+	fmt.Printf("total moves: %d \n", count)
 }
