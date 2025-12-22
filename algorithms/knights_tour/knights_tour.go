cd4b5ac79a49727c8e56b9502e7e545f4599418b
Viet Pham <vietpham102301@gmail.com>
2025-12-22 15:37:57 +0700

    ref: update knight's tour

--- algorithms/knights_tour/knights_tour.go
+++ algorithms/knights_tour/knights_tour.go
@@ -2,28 +2,37 @@ package main
 
 import "fmt"
 
-func knightsTour(x int, y int, board [][]int, step int, q *bool) {
-	xi := []int{2, 1, 2, 1, -2, -1, -2, -1}
-	yi := []int{1, 2, -1, -2, -1, -2, 1, 2}
+var (
+	xi = []int{2, 1, 2, 1, -2, -1, -2, -1}
+	yi = []int{1, 2, -1, -2, -1, -2, 1, 2}
+)
 
+func knightsTour(x int, y int, step int, board [][]int) bool {
 	boardSize := len(board)
+	board[x][y] = step
 
-	if step == boardSize*boardSize+1 {
-		*q = true
-		return
+	if step == boardSize*boardSize {
+		return true
 	}
 
-	for i := 0; (i < 8) && (*q == false); i++ {
+	for i := 0; i < 8; i++ {
 		xNext := x + xi[i]
 		yNext := y + yi[i]
-		if xNext >= 0 && yNext >= 0 && xNext < boardSize && yNext < boardSize && board[xNext][yNext] == 0 {
-			board[xNext][yNext] = step
-			knightsTour(xNext, yNext, board, step+1, q)
-			if !(*q) {
-				board[xNext][yNext] = 0
+
+		if isValid(xNext, yNext, board) {
+			if knightsTour(xNext, yNext, step+1, board) {
+				return true
 			}
 		}
 	}
+
+	board[x][y] = 0
+	return false
+}
+
+func isValid(x, y int, board [][]int) bool {
+	size := len(board)
+	return x >= 0 && y >= 0 && x < size && y < size && board[x][y] == 0
 }
 
 func initChessBoard(size int) [][]int {
@@ -31,26 +40,29 @@ func initChessBoard(size int) [][]int {
 	for i := 0; i < size; i++ {
 		res[i] = make([]int, size)
 	}
-
 	return res
 }
 
 func printChessBoard(board [][]int) {
 	for i := 0; i < len(board); i++ {
 		for j := 0; j < len(board[i]); j++ {
-			fmt.Printf("| %2d", board[i][j])
+			fmt.Printf("| %2d ", board[i][j])
 		}
 		fmt.Println("|")
 	}
 }
 
 func main() {
-	board := initChessBoard(8)
+	size := 8
+	board := initChessBoard(size)
 
-	q := new(bool)
+	// Bắt đầu tại vị trí (0, 0) hoặc (2, 2) với bước đi đầu tiên là 1
+	startX, startY := 2, 2
 
-	board[2][2] = 1
-	knightsTour(2, 2, board, 2, q)
-
-	printChessBoard(board)
+	if knightsTour(startX, startY, 1, board) {
+		fmt.Println("Đã tìm thấy đường đi:")
+		printChessBoard(board)
+	} else {
+		fmt.Println("Không tìm thấy giải pháp.")
+	}
 }
